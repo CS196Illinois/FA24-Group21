ba827c63ac7ef9d4a81f7fc0e1227761c3c3562a
lcenan2 <lcenan2@illinois.edu>
2024-10-17 21:44:57 -0500

    Update index.tsx

--- Project/StickerSmash/app/index.tsx
+++ Project/StickerSmash/app/index.tsx
@@ -1,21 +1,15 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+    <View 
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-});
