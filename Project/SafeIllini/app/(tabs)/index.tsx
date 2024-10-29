399951ccc2a483e769a7c0a5ef64859bd0e2bd07
pranaykarpuram <tanpranay@gmail.com>
2024-10-29 17:45:20 -0500

    nav bar updated

--- Project/SafeIllini/app/(tabs)/index.tsx
+++ Project/SafeIllini/app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from "react-native";
+import { database, auth } from "../../configs/firebaseConfig"
+import { ref, set, onValue } from 'firebase/database';
 
-export default function Index() {
+export default function Home() {
   return (
     <View
       style={{
@@ -9,7 +11,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello!!</Text>
     </View>
   );
 }
