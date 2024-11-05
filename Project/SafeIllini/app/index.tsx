a13e9cfbc31f22ecd9f7ca80eabcc320454db55a
shrutikde16 <de16@illinois.edu>
2024-11-05 00:27:08 +0000

    code to how to center the map

--- Project/SafeIllini/app/index.tsx
+++ Project/SafeIllini/app/index.tsx
@@ -1,17 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { database, auth } from "../configs/firebaseConfig"
 import { ref, set, onValue } from 'firebase/database';
+import MapView from 'react-native-maps';
+import { Platform } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello!!</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map}
+  initialRegion={{
+    latitude: 40.78825,
+    longitude: -88.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }}
+/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
