8f402c07ddbc1b46da42817cc09d624d98652818
Yash Ejjagiri <51063116+yaswant2403@users.noreply.github.com>
2024-11-19 21:29:44 +0000

    Reset StickerSmash folder to main branch

--- Project/StickerSmash/app/_layout.tsx
+++ Project/StickerSmash/app/_layout.tsx
@@ -1,16 +1,6 @@
 import { Stack } from 'expo-router';
-import { setStatusBarStyle } from "expo-status-bar";
-import { useEffect } from "react";
-
-import React from 'react';
 
 export default function RootLayout() {
-  useEffect(() => {
-    setTimeout(() => {
-      setStatusBarStyle("light");
-    }, 0);
-  }, [])
-
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
